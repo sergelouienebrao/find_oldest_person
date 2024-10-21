9ee0ff36bd0541ae537e9b1d286ea4f611f4f8ce
Serge <sergelouienebrao@gmail.com>
2024-10-21 23:46:45 +0800

    name and age inputs

--- find_oldest_person.py
+++ find_oldest_person.py
@@ -1,8 +1,17 @@
 #find oldest person
 
 #create empty list 
+people_name_and_age = {}
 
 #ask for name and age inputs
+while True:
+    try: 
+        name = input("Input name please: ")
+        age = int(input("Input age please: "))
+
+    except:
+        print("Invalid input, try again.")
+
 
 #make sure inputs are valid
 
