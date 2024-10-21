d15ba688cb1a90aef128c159af3e86ecc4252be1
Serge <sergelouienebrao@gmail.com>
2024-10-21 23:39:38 +0800

    pseudocode

---
+++ find_oldest_person.py
@@ -0,0 +1,17 @@
+#find oldest person
+
+#create empty list 
+
+#ask for name and age inputs
+
+#make sure inputs are valid
+
+#store inputs on array(dictionary) 
+
+#ask user if he wants to input another 
+
+#rank the age to find the oldest 
+
+
+
+
