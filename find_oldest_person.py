f585c37bf8035aad563bf9d29b6c86b8fd5539ee
Serge <sergelouienebrao@gmail.com>
2024-10-23 00:48:06 +0800

    done hehe

--- find_oldest_person.py
+++ find_oldest_person.py
@@ -3,7 +3,7 @@
 #create empty list 
 people_name_and_age = {}
 oldest_age = 0
-oldest_person= None
+person= ""
 #ask for name and age inputs
 while True:
     try:
@@ -12,25 +12,25 @@ while True:
 
 #store inputs on array(dictionary)
      people_name_and_age[name] = age
-
+     
 #rank the age to find the oldest
      if age > oldest_age:
         oldest_age = age
-        oldest_person = name 
+        person = name 
 
 #display name and age  
      print(f"Name: {name}, Age: {age}")
-
+    
 #make sure inputs are valid
     except:
-     print("Invalid input, try again.")
-
+       print("Invalid age, try again.")
+     
 #ask user if he wants to input another
     another_person = input("Do you want to add another person? Yes/No?: ")
     if another_person != "Yes":
       break
       
-
+print(f"The oldest person is {person} at age {oldest_age}.")
       
 
 
